040ffd7986f4f556207a3de5a54e645f823e5b75
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-07-26 21:01:08 -0300

    Adiciona body-parser, mongoose e conexão com MongoDB

--- app.js
+++ app.js
@@ -7,17 +7,38 @@ const usersRoute = require('./Routes/users');
 app.use('/', indexRoute);
 app.use('/users', usersRoute);
 
-// Primeiras requisições
-/*
-app.get('/', (req, res) => {
-    let obj = req.query;
-    return res.send({ message: `Http GET: Success! Response is :${obj.nome} with ${obj.idade}!` })
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userDb = 'admin';
+const passwordDb = 'admin';
+const url = `mongodb+srv://${userDb}:${passwordDb}@cluster0-lwoso.mongodb.net/test?retryWrites=true&w=majority`;
+
+const options = {
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    poolSize: 5,
+    useNewUrlParser: true
+};
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada com sucesso!');
 });
 
-app.post('/', (req, res) => {
-    return res.send({ message: 'Http POST: Success!' })
+mongoose.connection.on('error', (error) => {
+    console.log('Exception de Erro: ' + error);
 });
-**/
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Banco de dados offline');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(3000);
 
 module.exports = app;
