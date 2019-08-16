82e363bb7fb809a87b741e7815fc0a456c2a0c17
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2019-08-16 09:39:45 -0300

    Adiciona status HTTP nas mensagens de retorno da API

--- Routes/users.js
+++ Routes/users.js
@@ -13,79 +13,80 @@ const NOT_ENOUGH_FIELDS_MESSAGE = 'Por favor, informe todos os campos.';
 const WRONG_PASSWORD_MESSAGE = 'Senha inválida.';
 const APP_SECRET = 'user_password_jwt_123456';
 
-const createUserToken = (userId) => {
-    return jwt.sign({ id: userId }, APP_SECRET, { expiresIn: '7d' });
-}
+const createUserToken = userId => {
+  return jwt.sign({ id: userId }, APP_SECRET, { expiresIn: '7d' });
+};
 
 router.get('/all', async (req, res) => {
-    try {
-        const users = await User.find({});
-        return res.send(users);
-    } catch (error) {
-        return res.send({ error: USERS_ERROR_MESSAGE });
-    }
-
+  try {
+    const users = await User.find({});
+    return res.send(users);
+  } catch (error) {
+    return res.status(400).send({ error: USERS_ERROR_MESSAGE });
+  }
 });
 
 router.post('/create', async (req, res) => {
-    const request = req.body;
-    const { email, password, name } = request;
-    if (!email || !password || !name) {
-        return res.send({ error: NOT_ENOUGH_FIELDS_MESSAGE });
+  const request = req.body;
+  const { email, password, name } = request;
+  if (!email || !password || !name) {
+    return res.status(400).send({ error: NOT_ENOUGH_FIELDS_MESSAGE });
+  }
+  try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ message: USER_EXISTS_MESSAGE });
     }
-    try {
-        if (await User.findOne({ email })) {
-            return res.send({ message: USER_EXISTS_MESSAGE });
-        }
-        const user = await User.create(request);
-        user.password = undefined;
-        return res.send({ user, token: createUserToken(user.id), auth_data: res.auth_data });
-    } catch (error) {
-        return res.send({ error: USER_NOT_CREATED_MESSAGE });
-    }
+    const user = await User.create(request);
+    user.password = undefined;
+    return res
+      .status(201)
+      .send({ user, token: createUserToken(user.id), auth_data: res.auth_data });
+  } catch (error) {
+    return res.status(400).send({ error: USER_NOT_CREATED_MESSAGE });
+  }
 });
 
 // Controller utilizando URI Param
 router.get('/find/:email', async (req, res) => {
-    const { email } = req.params;
-    try {
-        const user = await User.findOne({ email });
-        return res.send(user);
-    } catch (error) {
-        return res.send({ error: USER_NOT_FOUND_MESSAGE });
-    }
+  const { email } = req.params;
+  try {
+    const user = await User.findOne({ email });
+    return res.send(user);
+  } catch (error) {
+    return res.status(400).send({ error: USER_NOT_FOUND_MESSAGE });
+  }
 });
 
 // Controller utilizando Query Param
 router.get('/find', async (req, res) => {
-    const { email } = req.query;
-    try {
-        const user = await User.findOne({ email });
-        return res.send(user);
-    } catch (error) {
-        return res.send({ error: USER_NOT_FOUND_MESSAGE });
-    }
+  const { email } = req.query;
+  try {
+    const user = await User.findOne({ email });
+    return res.send(user);
+  } catch (error) {
+    return res.status(400).send({ error: USER_NOT_FOUND_MESSAGE });
+  }
 });
 
 router.post('/auth', async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.send({ error: NOT_ENOUGH_FIELDS_MESSAGE });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send({ error: NOT_ENOUGH_FIELDS_MESSAGE });
+  }
+  try {
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(400).send({ error: USER_NOT_EXISTS_MESSAGE });
     }
-    try {
-        const user = await User.findOne({ email }).select('+password');
-        if (!user) {
-            return res.send({ error: USER_NOT_EXISTS_MESSAGE });
-        }
-        const password_ok = await bcrypt.compare(password, user.password);
-        if (!password_ok) {
-            return res.send({ error: WRONG_PASSWORD_MESSAGE });
-        }
-        user.password = undefined;
-        return res.send({ user, token: createUserToken(user.id), auth_data: res.auth_data });
-    } catch (error) {
-        return res.send({ error: USER_NOT_FOUND_MESSAGE });
+    const password_ok = await bcrypt.compare(password, user.password);
+    if (!password_ok) {
+      return res.status(401).send({ error: WRONG_PASSWORD_MESSAGE });
     }
+    user.password = undefined;
+    return res.send({ user, token: createUserToken(user.id), auth_data: res.auth_data });
+  } catch (error) {
+    return res.status(400).send({ error: USER_NOT_FOUND_MESSAGE });
+  }
 });
 
 module.exports = router;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,17 +5,17 @@ const USER_INVALID_TOKEN = 'Token inválida.';
 const APP_SECRET = 'user_password_jwt_123456';
 
 const auth = (req, res, next) => {
-    const token_header = req.headers.auth;
-    if (!token_header) {
-        return res.send({ message: USER_UNAUTHORIZED });
+  const token_header = req.headers.auth;
+  if (!token_header) {
+    return res.status(401).send({ message: USER_UNAUTHORIZED });
+  }
+  jwt.verify(token_header, APP_SECRET, (error, decoded) => {
+    if (error) {
+      res.status(401).send({ message: USER_INVALID_TOKEN });
     }
-    jwt.verify(token_header, APP_SECRET, (error, decoded) => {
-        if (error) {
-            res.send({ message: USER_INVALID_TOKEN });
-        }
-        res.locals.auth_data = decoded;
-        return next();
-    });
-}
+    res.locals.auth_data = decoded;
+    return next();
+  });
+};
 
 module.exports = auth;
